4671edadf01f3faf6ec635f55abf97dc760e2eab
Paraform <code@paraform.studio>
2024-01-22 12:40:37 +0000

    fix: remove edge runtime from api

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,10 +1,6 @@
 import { app } from "@/server/api";
 import { handle } from "hono/vercel";
 
-export const config = {
-  runtime: "edge",
-};
-
 export const GET = handle(app);
 export const POST = handle(app);
 
