46f8015ff7f1200fd3003cb63a73b4d6d51024b4
Paraform <code@paraform.studio>
2024-01-22 18:51:16 +0000

    chore: add not found & error to api

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,25 +3,3 @@ import { handle } from "hono/vercel";
 
 export const GET = handle(app);
 export const POST = handle(app);
-
-// export type GlobalErrorResponse = {
-//   error: {
-//     message: Error;
-//     status: number;
-//   };
-// };
-
-// app.onError(async (err, c) => {
-//   return c.json<GlobalErrorResponse>({
-//     error: {
-//       message: err,
-//       status: 500,
-//     },
-//   });
-// });
-
-// app.notFound(async (c) => {
-//   return c.text(
-//     `Oops! There is nothing here! 404 Not Found.                                                                          `
-//   );
-// });
--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -2,9 +2,29 @@ import { userRouter } from "./routers/user";
 import { taskRouter } from "./routers/task";
 import { context } from "./context";
 
+export type GlobalErrorResponse = {
+  error: {
+    message: Error["message"];
+    status: number;
+  };
+};
+
 export const app = context
   .basePath("/api")
   .route("/user", userRouter)
-  .route("/task", taskRouter);
+  .route("/task", taskRouter)
+  .onError(async (err, c) => {
+    return c.json<GlobalErrorResponse>({
+      error: {
+        message: err.message,
+        status: 500,
+      },
+    });
+  })
+  .notFound(async (c) => {
+    return c.text(
+      `Oops! There is nothing here! 404 Not Found.                                                                          `,
+    );
+  });
 
 export type AppType = typeof app;
